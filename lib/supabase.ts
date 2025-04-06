cb0da6a89b2332e98a0fc43f6508a85882709d6e
Lina Mei <linamei0616@gmail.com>
2025-04-05 22:23:06 -0500

    Added layer of security

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xxcztxyzskewgernopyh.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4Y3p0eHl6c2tld2dlcm5vcHloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4NzY3MjQsImV4cCI6MjA1OTQ1MjcyNH0.X9FjnB2FDtfGf0m4P6yNsyiQIL871zfe73kt_bfiQVA'
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
+}
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
+}
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+) 
